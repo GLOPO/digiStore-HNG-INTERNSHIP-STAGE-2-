3806cabf78d234ad26979a7a1dea5278369c047b
GLOPO <gloryohuali@gmail.com>
2024-07-08 16:28:32 +0100

    first commit

---
+++ app/components/Products.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { watches, headphones } from "../../db";
+import Image from "next/image";
+import cart from '../assets/cart.jpg'
+
+const Products = () => {
+  return (
+    <main className="container">
+      <div className="flex justify-between items-baseline">
+        <h1 className="my-4">Product Listing</h1>
+        <div className="w-5 rounded-full" role="button">
+        <Image src={cart} alt="cart"/>
+        </div>
+      </div>
+    {/* watches  */}
+      <main>
+        <h5 className="my-3">Smart Watches</h5>
+        <div className="flex items-center justify-between">
+          {watches.map((watch) => {
+            const { id, name, image } = watch;
+
+            return (
+              <div key={id}>
+                <div className="text-white border rounded w-48 bg-white">
+                  <Image src={watch.image} alt="watch-1" />
+                </div>
+
+                <div>
+                  <p className="text-black font-semibold mb-0 py-1">{name}</p>
+                  <p className="mb-0 font-semibold">50,000</p>
+                  <button className="text-white bg-purple-900 my-1 px-3 py-1 border rounded font-semibold">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+
+      {/* headphones  */}
+      <main className="my-5">
+      <h5 className="my-3">Headphones and Airpods</h5>
+      <div className='flex items-center justify-between'>
+        {headphones.map((headphone) => {
+          const {id, name, image} = headphone
+
+          return(
+            <div key={id}>
+              <div className='text-white border rounded w-48 bg-white'>
+              <Image src={image} alt='watch-1'/>
+              </div>
+
+              <div>
+                <p className='text-black font-semibold mb-0 py-1'>{name}</p>
+                <p className='mb-0 font-semibold'>50,000</p>
+                <button className='text-white bg-purple-900 my-1 px-3 py-1 border rounded font-semibold'>Add to cart</button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </main>
+    </main>
+  );
+};
+
+export default Products;
---
+++ db.js
@@ -0,0 +1,22 @@
+import watch1 from './app/assets/watch1.png'
+import watch2 from './app/assets/watch2.jpg'
+import watch3 from './app/assets/watch3.jpg'
+import watch4 from './app/assets/watch4.jpg'
+import set1 from './app/assets/headset1.jpg'
+import set2 from './app/assets/headset2.jpg'
+import set3 from './app/assets/headset3.jpg'
+import set4 from './app/assets/headset4.jpg'
+
+export let watches = [
+  {id: 1, name: 'Stellar Gear', image: watch1},
+  {id: 2, name: 'Zenith Wave', image: watch2},
+  {id: 3, name: 'Fusion Chrono', image: watch3},
+  {id: 4, name: 'Quantum Pulse', image: watch4},
+]
+
+export let headphones = [
+  {id: 1, name: 'Bluetooth headphones', image: set1},
+  {id: 2, name: 'Echo Flux', image: set2},
+  {id: 3, name: 'Sonic Breeze', image: set3},
+  {id: 4, name: 'Bass Surge', image: set4}
+]
